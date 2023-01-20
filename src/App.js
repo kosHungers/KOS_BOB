3559741b0be946cfa49ad75d3fe44466ba1cedb5
Seulki Yun <icstuckyi@gmail.com>
2023-01-20 14:57:46 +0900

    🔥 Remove: 불필요한 파일 삭제 및 index.html 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { useContext } from 'react';
 import { DiningListContext } from './Context/DiningListContext';
+import BasicExample from './Components/Card/Card';
 
 function App() {
   const { state } = useContext(DiningListContext);
 
   console.log(state);
-  return <div></div>;
+  return (
+    <div>
+      <BasicExample />
+    </div>
+  );
 }
 
 export default App;
