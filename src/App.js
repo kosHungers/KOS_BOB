a4d49b09421153d0805740e77cab78229b348c20
seongjin <kssj9056@gmail.com>
2023-01-24 22:48:48 +0900

    ✨ Feat: 검색 기능 구현 완료
    css 작업은 아직 미완입니다.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { useContext } from 'react';
-import { DiningListContext } from './Context/DiningListContext';
+// import { useContext } from 'react';
+// import { DiningListContext } from './Context/DiningListContext';
 import BasicExample from './Components/Card/Card';
 import { Container } from 'react-bootstrap';
 import Search from './Components/Search/Search';
 
 function App() {
-  const { state } = useContext(DiningListContext);
+  // const { state } = useContext(DiningListContext);
 
   // console.log(state);
   return (
     <>
       <Container>
         <Search/>
-        <BasicExample />;
+        <BasicExample />
       </Container>
     </>
   );
